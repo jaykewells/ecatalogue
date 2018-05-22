b6988d86904580b9a4a53eeaf076284785c242f7
Jayke Wells <jaykewells@gmail.com>
2018-05-22 08:24:34 -0500

    Initial commit

---
+++ src/main/java/org/launchode/ecatalogue/EcatalogueApplication.java
@@ -0,0 +1,12 @@
+package org.launchode.ecatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcatalogueApplication.class, args);
+	}
+}
